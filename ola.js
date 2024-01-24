4c53bd1ba0d1069249e70113e3158d83dcaef03b
mbertuci <manubertuci@gmail.com>
2024-01-24 10:43:46 -0400

    adiciona a primeira versão do meu projeto

---
+++ ola.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta) 
+} 
+
+app.listen(porta, mostraPorta) 
